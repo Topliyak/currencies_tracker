b0a361cf516db0316ec35476ff819d172701bc58
Amir Mustafaev <amir.mustafaev@mentalsea.com>
2023-09-09 12:22:53 +0300

    request getting trades

--- app.py
+++ app.py
@@ -27,13 +27,27 @@ class Price:
     
     
 @dataclass
-class MarketRequests:
-    timestamp: int
-    asks: List[Price]
-    bids: List[Price]
+class Trade:
+    """
+        id
+        price - цена
+        volume - сумма в базовой валюте
+        funds - сумма в валюте котировки
+        market - id рынка
+        created_at - время сделки
+    """
+    
+    id: int
+    price: str
+    volume: str
+    funds: str 
+    market: str
+    created_at: str
 
 
 def get_markets() -> List[Market]:
+    """ https://garantexio.github.io/?shell#47ff48632f """
+    
     url = 'https://garantex.org/api/v2/markets'
     
     response = requests.get(url).text
@@ -42,17 +56,31 @@ def get_markets() -> List[Market]:
     return markets
 
 
-def get_market_requests(market_id: str) -> MarketRequests:
-    url = 'https://garantex.org/api/v2/depth'
-    market_param = 'market'
+def get_trades(market_id: str) -> List[Trade]:
+    """ https://garantexio.github.io/?shell#f00f8079b3 """
     
-    params = {market_param: market_id}
+    url = 'https://garantex.org/api/v2/trades'
+    
+    market_param = 'market'
+    timestamp_param = 'timestamp'
+    limit_param = 'limit'
+    
+    params = {
+        market_param: market_id,
+        limit_param: 10,
+    }
+    
     response = requests.get(url, params)
     
-    market_requests: MarketRequests = json.loads(response.text)
+    trades = [Trade(**tdict) for tdict in json.loads(response.text)]
     
-    return market_requests
+    return trades
 
 
-markets = get_markets()
-[get_market_requests(m.id) for m in markets]
+if __name__ == '__main__':
+    markets = get_markets()
+    trades = {m.id: get_trades(m.id) for m in markets}
+    
+    for m_id, t in trades.items():
+        print(m_id, t)
+        print()
