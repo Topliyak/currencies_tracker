86e9be722074a3936d288621708144ea74aed284
Amir Mustafaev <amir.mustafaev@mentalsea.com>
2023-09-17 15:27:55 +0300

    add home page

--- server/app.py
+++ server/app.py
@@ -9,6 +9,15 @@ from service.app import (
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET'])
+def home():
+    return """
+                <a href="best_price?market_id=LTCBTC">best prices</a>
+                <hr>
+                <a href="average_price?market_id=USDTRUB">average prices</a>
+    """
+
+
 @app.route('/best_price', methods=['GET'])
 def best_price():
     market_id = request.args.get('market_id')
@@ -17,7 +26,7 @@ def best_price():
     return best_price
 
 
-@app.route('/average_price')
+@app.route('/average_price', methods=['GET'])
 def average_price():
     market_id = request.args.get('market_id')
     average_price = get_average_price(market_id)
